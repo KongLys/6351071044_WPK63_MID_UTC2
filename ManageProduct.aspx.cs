c0f470807d565c22e087a9b79b5e5721729a5b6a
Nguyen Tran Cong Ly <trancongly204@gmail.com>
2024-09-27 16:55:16 +0700

    add new button

--- ManageProduct.aspx.cs
+++ ManageProduct.aspx.cs
@@ -16,7 +16,24 @@ namespace de1
 
         protected void ButtonAddNew_Click(object sender, EventArgs e)
         {
+            string fileName = "";
+            if (FileUploadPicture.HasFile)
+            {
+                fileName = FileUploadPicture.FileName;
+                FileUploadPicture.SaveAs(Server.MapPath("~/images/Courses/" + fileName));
+            }
+            QLKhoaHocEntities context = new QLKhoaHocEntities();
+            Course p = new Course();
+            p.Name = TextBoxName.Text;
+            p.Duration = int.Parse(TextBoxDuration.Text);
+            p.CatID = int.Parse(DropDownListCategory.SelectedValue);
+            p.Description = TextBoxDescription.Text;
+            p.ImageFilePath = fileName;
 
+            context.Courses.Add(p);
+            context.SaveChanges();
+            BindGridView(); //Cap nhat du lieu lai trong grid view
+            Page.Master.DataBind();
         }
     }
 }
